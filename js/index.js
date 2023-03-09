756226daf20a25a7a8f88aa21180e504bf6b4369
RodrigoBuda <rodrigo.buda@outlook.com>
2023-03-09 07:54:40 -0300

    commit_task_3

--- js/index.js
+++ js/index.js
@@ -1,20 +1,75 @@
 const contenedorTarjetas = document.getElementById("contenedor");
+let buscador = document.getElementById("buscador");
 
-let tarjetas = "";
-
-for (const e of events) {
-  tarjetas += `
+function dibujaTarjetas(eventos) {
+  let tarjetas = "";
+  for (const e of eventos) {
+    tarjetas += `
 <div class="col-sm-6 col-md-4 ml-5">
   <div class="card" style="width: 18rem">
        <img src="${e.image}" alt="" />
         <div class="card-body">
+        <div class="cont-h1-card">
             <h1> ${e.name} </h1>
+        </div>
+        <div class="cont-description">
             <p class="card-text">${e.description}</p>
-            <a href="#" class="boton">
+           </div>
+           <div class="cont-category">
+           <p class="card-text">Category: ${e.category} </p>
+           </div>
+            <p>${e.date}</p>
+            <a href="./details.html?id=${e._id}" id="botonJs" class="boton">
             See More
             </a>
         </div>
    </div>
 </div>`;
+  }
+  return tarjetas;
 }
-contenedorTarjetas.innerHTML = tarjetas;
+
+function actualizarTarjetas(eventos) {
+  contenedorTarjetas.innerHTML = dibujaTarjetas(eventos);
+}
+
+actualizarTarjetas(events);
+
+buscador.addEventListener("keyup", () => {
+  let filteredCards = events.filter((event) =>
+    event.name.toLowerCase().includes(buscador.value.trim().toLowerCase())
+  );
+  actualizarTarjetas(filteredCards);
+});
+
+/*--------filtrar----------*/
+
+const filterEventsByCategory = (arrayCategories, arrayEvents = events) => {
+  let filteredEvents = [];
+  arrayCategories.forEach((category) => {
+    arrayEvents.forEach((event) => {
+      if (event.category == category) {
+        filteredEvents.push(event);
+      }
+    });
+  });
+  return filteredEvents;
+};
+
+const arrCategorySelected = () => {
+  let selection = [];
+
+  arrCategories.forEach((category) => {
+    let selector = document.getElementById(category);
+    if (selector.checked) {
+      selection.push(category);
+    }
+  });
+
+  let arrayFinal = [];
+  if (selection.length != 0) {
+    actualizarTarjetas(filterEventsByCategory(selection));
+  } else {
+    actualizarTarjetas(events);
+  }
+};
